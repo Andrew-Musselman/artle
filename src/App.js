5c3b3640706137b8bd1d722a712b7019aa1dea01
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-20 16:54:56 -0600

    Chain methods to get information on each individual object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import getData from './apiCall';
 import './App.css';
 
 class App extends Component {
@@ -12,23 +13,24 @@ class App extends Component {
 
   getObjects = async () => {
     const artistPath = 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=Renoir'
-    const response = await fetch(artistPath);
-    return await response.json();
+    const response = await getData(artistPath);
+    return await response
   }
 
   getTopSixObjects = async () => {
-    const objectIds = await this.getObjects()
-    const topSixObjects = await objectIds.objectIDs.slice(0, 6)
+    const objects = await this.getObjects()
+    const topSixObjects = await objects.objectIDs.slice(0, 6)
     return await topSixObjects
    }
 
-   getObjectDetails = async () => {
-     const objectsMapped = []
+  getObjectDetails = async () => {
+    const objectsMapped = []
     const objects = await this.getTopSixObjects()
-    const objectDetails = await objects.forEach(object => {
-      return fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${object}`).then(data => console.log(data))
-     })
-     console.log(await objectDetails)
+    for (const objectID of objects) {
+      const singleObjectDetails = await getData(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`)
+      objectsMapped.push(singleObjectDetails)
+    }
+     console.log(await objectsMapped)
    }
 
   componentDidMount() {
