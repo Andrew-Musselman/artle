22eaddcc094d21e12ba28f956aedbc6222427cc4
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-22 09:47:52 -0600

    Render image carousel on correct screen

--- src/CorrectScreen.js
+++ src/CorrectScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import ArtistCard from './ArtistCard';
+import ImageCarousel from './ImageCarousel';
 import './CorrectScreen.css';
 
-const CorrectScreen = () => {
+const CorrectScreen = ({ artistName, artistBio, images }) => {
 
     return (
         <>
             <h2>Congrats, you got it</h2> 
+            <ImageCarousel images={images} />
+            <ArtistCard artistName={artistName} artistBio={artistBio} />
         </>
 
     )
