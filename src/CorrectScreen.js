52ed24f2f1ac19ffb1d4c96afb95cecd81743ad4
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-22 09:34:42 -0600

    Write basic correct screen component

---
+++ src/CorrectScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CorrectScreen.css';
+
+const CorrectScreen = () => {
+
+    return (
+        <>
+            <h2>Congrats, you got it</h2> 
+        </>
+
+    )
+}
+
+export default CorrectScreen;
