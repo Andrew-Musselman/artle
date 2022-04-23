49b90da6ac39eec71082da9a2079c9b2a0782d11
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-23 10:27:55 -0600

    Remove unused link to stylesheet

--- src/EndGameScreen.js
+++ src/EndGameScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ArtistCard from './ArtistCard';
 import ImageCarousel from './ImageCarousel';
 import EndGameMessage from './EndGameMessage'
-// import './CorrectScreen.css';
+
 
 const EndGameScreen = ({ correctGuess, artistName, artistBio, images }) => {
 
