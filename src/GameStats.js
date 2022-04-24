8853edf4d4aa82738c42be4a5cf7167395712ab3
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-24 13:56:45 -0600

    Add game stats component

---
+++ src/GameStats.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const GameStats = () => {
+    return (
+        <>
+            <h2>Game Stats</h2>
+            <p></p>
+            <p></p>
+        </>
+    )
+}
+
+export default GameStats;
+
+GameStats.propTypes = {
+
+}
