32fde78d0f9c06a4dcc9c3c095debcec38391ca7
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-24 13:56:18 -0600

    Add an about section

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <>
+            <p>Artle is a game to help you learn more about art history by guessing the artist based on paintings</p>
+            <p>You will have six attempts to guess the correct artist</p>
+            <p>Each failed attempt will give you a new painting by that artist</p>
+        </>
+    )
+}
+
+export default About;
