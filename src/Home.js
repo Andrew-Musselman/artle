3de065c4671afe54e56a556d6ff860afcfd90a43
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-24 14:59:54 -0600

    Add proptypes

--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,12 @@ import GameScreen from './GameScreen';
 import GuessForm from './GuessForm';
 import EndGameScreen from './EndGameScreen';
 
-const Home = ({hasError, gameOver, errorMessage, correctGuess, viewableImages, artistName, artistBio, images, playGame}) => {
+const Home = ({hasError, gameOver, errorMessage, correctGuess, viewableImages, artistName, artistBio, images, titles, playGame}) => {
     return (
         <>
             {hasError && <h2 className='error'>{errorMessage}</h2>}
             {!correctGuess && !gameOver && <GameScreen images={viewableImages} /> } 
-            {gameOver && <EndGameScreen correctGuess={correctGuess} artistName={artistName} artistBio={artistBio} images={images}/>}
+            {gameOver && <EndGameScreen correctGuess={correctGuess} artistName={artistName} artistBio={artistBio} images={images} titles={titles} />}
             {!hasError &&<GuessForm playGame={playGame} /> }
         </>
     )
@@ -26,5 +26,6 @@ Home.propTypes = {
     artistName: PropTypes.string.isRequired,
     artistBio: PropTypes.string.isRequired,
     images: PropTypes.arrayOf(string).isRequired,
+    titles: PropTypes.arrayOf(string).isRequired,
     playGame: PropTypes.func.isRequired
 }
