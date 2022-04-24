1764ea6fbf7417a904489c22f6b62bf3647934a9
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-24 16:00:30 -0600

    Add artist data to search through

---
+++ src/artistsData.js
@@ -0,0 +1,4 @@
+const artists = ['Auguste Renoir', 'David Roentgen', "Edgar Degas", "Georges Jacob", 
+"Auguste Rodin", "Francisco Goya", "Johannes Vermeer", "Vincent van Gogh", "Winslow Homer", 
+"Botticelli", "El Greco", "Caravaggio (Michelangelo Merisi)", "Rembrandt", "Edouard Manet", 
+"Paul Cézanne", "Jan van der Heyden", "Paul Gauguin", "Thomas Sully", "Eugène Delacroix"]
